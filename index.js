f310ee97fb32fadcf2f6214e976f7196ca8c6aee
Patrick Cremin <pwcremin@gmail.com>
2017-01-27 11:00:54 -0600

    added router

--- index.js
+++ index.js
@@ -1,130 +1,141 @@
+"use strict";
+
+let express = require( 'express' );
 let request = require( "request" );
 
-//let cloudantUrl = 'https://user:pass@xxxx.cloudant.com';
-//let dotenv = require( 'dotenv' ).config();
-//let cloudantUrl = process.env.CLOUDANT;
+let router = express.Router();
 
+let CLOUDANT_URL = null;
 
-class CloudantKeyGenerator {
-
-    //cloudantUrl = 'https://user:pass@xxxx.cloudant.com';
-    init( cloudantUrl )
+function createDatabase( data )
+{
+    return new Promise( ( resolve, reject ) =>
     {
-        this.cloudantUrl = cloudantUrl;
-    }
+        let url = CLOUDANT_URL + '/' + data.dbname;
 
-    createDatabase( data )
-    {
-        return new Promise( ( resolve, reject ) =>
+        request( { url: url, method: 'PUT' }, function ( err, response )
         {
-            let url = cloudantUrl + '/' + data.dbname;
-
-            request( { url: url, method: 'PUT' }, function ( response )
+            if ( err )
             {
-                let DB_CREATION_SUCCESS = 201;
-                let DATABASE_EXISTS = 412;
+                reject( err )
+            }
 
-                if ( response.statusCode != DB_CREATION_SUCCESS && response.statusCode != DATABASE_EXISTS )
-                {
-                    reject( 'db creation failed: ' + response.statusCode );
-                }
+            let DB_CREATION_SUCCESS = 201;
+            let DATABASE_EXISTS = 412;
 
-                resolve( data );
-            } );
+            if ( response.statusCode != DB_CREATION_SUCCESS && response.statusCode != DATABASE_EXISTS )
+            {
+                reject( 'db creation failed: ' + response.statusCode );
+            }
+
+            resolve( data );
+        } );
+    } )
+
+}
+
+function generateApiKey( data )
+{
+    return new Promise( ( resolve, reject ) =>
+    {
+        let url = CLOUDANT_URL + '/_api/v2/api_keys';
+
+        /* response looks like
+         {
+         "password": "YPNCaIX1sJRX5upaL3eqvTfi",
+         "ok": true,
+         "key": "blentfortedsionstrindigl"
+         }
+         */
+
+        request( { url: url, method: 'POST' }, function ( err, response )
+        {
+            if ( err )
+            {
+                reject( err )
+            }
+
+            if ( response.statusCode !== 201 )
+            {
+                reject( 'api generation failed: ' + response.statusCode );
+            }
+
+            let apiKey = JSON.parse( response.body );
+
+            data.key = apiKey.key;
+            data.password = apiKey.password;
+
+            resolve( data );
         } )
+    } )
 
-    }
+}
 
-    generateApiKey( data )
+function assignKeyToDatabase( data )
+{
+    return new Promise( ( resolve, reject ) =>
     {
-        return new Promise( ( resolve, reject ) =>
+        // Docs: https://docs.cloudant.com/authorization.html
+
+        let url = CLOUDANT_URL + '/_api/v2/db/' + data.dbname + '/_security';
+
+        let body = { cloudant: {} };
+        body.cloudant[ data.key ] = [
+            "_reader",
+            "_writer"
+        ];
+
+        body.cloudant[ 'nobody' ] = [];
+
+        return request( {
+            url: url,
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify( body )
+        }, function ( err, response )
         {
-            let url = cloudantUrl + '/_api/v2/api_keys';
-
-            /* response looks like
-             {
-             "password": "YPNCaIX1sJRX5upaL3eqvTfi",
-             "ok": true,
-             "key": "blentfortedsionstrindigl"
-             }
-             */
-
-            request( { url: url, method: 'POST' }, function ( response )
+            if ( err )
             {
-                if ( response.statusCode !== 201 )
-                {
-                    reject( 'api generation failed: ' + response.statusCode );
-                }
-
-                let apiKey = JSON.parse( response.body );
-
-                data.key = apiKey.key;
-                data.password = apiKey.password;
-
-                resolve( data );
-            } )
-        } )
-
-    }
-
-    assignKeyToDatabase( data )
-    {
-        return new Promise( ( resolve, reject ) =>
-        {
-            // Docs: https://docs.cloudant.com/authorization.html
-
-            let url = cloudantUrl + '/_api/v2/db/' + data.dbname + '/_security';
-
-            let body = { cloudant: {} };
-            body.cloudant[ data.key ] = [
-                "_reader",
-                "_writer"
-            ];
-
-            body.cloudant[ 'nobody' ] = [];
-
-            return request( {
-                url: url,
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify( body )
-            }, function ( response )
-                {
-                    if ( response.statusCode !== 200 )
-                    {
-                        reject( 'database security update failed: ' + response.statusCode );
-                    }
-
-                    resolve( data );
-                } );
-        });
-    }
-
-    genkey( req, res )
-    {
-        let dbname = req.query.dbname;
-
-        if ( !dbname )
-        {
-            res.status( 500 ).send();
-            return;
-        }
-
-        let data = { dbname: dbname };
-
-        this.createDatabase( data )
-            .then( this.generateApiKey.bind( this ) )
-            .then( this.assignKeyToDatabase.bind( this ) )
-            .then( function ( data )
+                reject( err )
+            }
+            
+            if ( response.statusCode !== 200 )
             {
-                res.json( data );
-            } )
-            .catch( function ( err )
-            {
-                res.status( 500 ).send( err );
-            } )
-    };
+                reject( 'database security update failed: ' + response.statusCode );
+            }
+
+            resolve( data );
+        } );
+    } );
 }
 
 
-module.exports = new CloudantKeyGenerator();
+router.get( '/', ( req, res ) =>
+{
+    let dbname = req.query.dbname;
+
+    if ( !dbname )
+    {
+        res.status( 500 ).send();
+        return;
+    }
+
+    let data = { dbname: dbname };
+
+    createDatabase( data )
+        .then( generateApiKey )
+        .then( assignKeyToDatabase )
+        .then( function ( data )
+        {
+            res.json( data );
+        } )
+        .catch( function ( err )
+        {
+            res.status( 500 ).send( err );
+        } )
+} );
+
+module.exports = {
+    router,
+    init: cloudantUrl => CLOUDANT_URL = cloudantUrl
+};
